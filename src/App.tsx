cb6d6ffc961053d94452ac6389510f832702557b
faisalm29 <faisal.muhammad2911@gmail.com>
2026-01-22 19:33:56 +0700

    implement data provider

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,15 @@ import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import {BrowserRouter, Outlet, Route, Routes} from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
 import Dashboard from "@/pages/dashboard.tsx";
-import {BookOpen, Home} from "lucide-react";
-import {Layout} from "@/components/refine-ui/layout/layout.tsx";
+import { BookOpen, Home } from "lucide-react";
+import { Layout } from "@/components/refine-ui/layout/layout.tsx";
 import SubjectsList from "@/pages/subjects/list.tsx";
 import SubjectsCreate from "@/pages/subjects/create.tsx";
 
@@ -34,31 +34,33 @@ function App() {
                 projectId: "O1o2rA-N7qATw-Gvi5M3",
               }}
               resources={[
-                  {
-                      name: "dashboard",
-                      list: "/",
-                      meta: {label: "Home", icon: <Home />,}
-                  },
-                  {
-                      name: "subjects",
-                      list: "/subjects",
-                      create: "/subjects/create",
-                      meta: {label: "Subjects", icon: <BookOpen/>}
-                  }
+                {
+                  name: "dashboard",
+                  list: "/",
+                  meta: { label: "Home", icon: <Home /> },
+                },
+                {
+                  name: "subjects",
+                  list: "/subjects",
+                  create: "/subjects/create",
+                  meta: { label: "Subjects", icon: <BookOpen /> },
+                },
               ]}
             >
               <Routes>
-                  <Route element={
-                      <Layout>
-                          <Outlet />
-                      </Layout>
-                  }>
-                    <Route path="/" element={<Dashboard />}/>
-                      <Route path="subjects">
-                        <Route index element={<SubjectsList />}/>
-                        <Route path="create" element={<SubjectsCreate />}/>
-                      </Route>
+                <Route
+                  element={
+                    <Layout>
+                      <Outlet />
+                    </Layout>
+                  }
+                >
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="subjects">
+                    <Route index element={<SubjectsList />} />
+                    <Route path="create" element={<SubjectsCreate />} />
                   </Route>
+                </Route>
               </Routes>
               <Toaster />
               <RefineKbar />
